6bbb0a4c8fce4d5a9b43281a338c4fa7980b5c5a
wonbin choi <fnffnqls@naver.com>
2023-07-17 15:42:46 +0900

    feat: Users 모델 생성

---
+++ models/users.js
@@ -0,0 +1,68 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Reservations, Reviews, Posts }) {
+      // define association here
+      this.hasMany(Reservations, {
+        foreignKey: 'User_id',
+        onDelete: 'cascade',
+        hooks: true,
+      });
+      this.hasMany(Reviews, {
+        foreignKey: 'User_id',
+        onDelete: 'cascade',
+        hooks: true,
+      });
+      this.hasMany(Posts, {
+        foreignKey: 'User_id',
+        onDelete: 'cascade',
+        hooks: true,
+      });
+    }
+  }
+  Users.init(
+    {
+      user_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      login_id: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      nickname: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Users',
+    }
+  );
+  return Users;
+};
