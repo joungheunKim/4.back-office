f5980456d173e58e1794919a94e33251b694df38
yuni416 <131755987+yuni416@users.noreply.github.com>
2023-07-19 19:25:42 +0900

    layered architecture

---
+++ services/reviewsService.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+const env = process.env;
+
+const ReviewsRepository = require('../repositories/reviewsRepository');
+
+class ReviewsService {
+  reviewsRepository = new ReviewsRepository();
+
+  get = async (sitter_id) => {
+    const sitter = await this.reviewsRepository.findById([
+      { sitter_id: sitter_id },
+    ]);
+
+    if (!sitter) throw { code: 411, message: '조건에 맞는 시터가 없습니다.' };
+
+    await this.reviewsRepository.findById({
+      where: { sitter_id },
+      attributes: [
+        'review_id',
+        'sitter_id',
+        'nickname',
+        'content',
+        'rate',
+        'createdAt',
+      ],
+      order: [['createdAt', 'DESC']],
+    });
+    return { code: 201, message: '리뷰 검색에 성공하였습니다.' };
+  };
+
+  post = async (nickname, user_id, sitter_id, content, rate, review_id) => {
+    const sitter = await this.reviewsRepository.findById([
+      { sitter_id: sitter_id },
+    ]);
+
+    if (!sitter) throw { code: 411, message: '조건에 맞는 시터가 없습니다.' };
+
+    const rateRegex = /^[1-5]$/g;
+    if (!rateRegex.test(rate))
+      throw {
+        code: 412,
+        errorMessage: '평점은 1점에서 5점까지 부여할 수 있습니다.',
+      };
+
+    await this.reviewsRepository.create({
+      Nickname: nickname,
+      User_id: user_id,
+      Sitter_id: sitter_id,
+      content,
+      rate,
+      review_id,
+    });
+    return { code: 201, message: '리뷰를 생성하였습니다.' };
+  };
+
+  put = async (user_id, sitter_id, content, rate, review_id) => {
+    const review = await this.reviewsRepository.findById([
+      { review_id: review_id },
+    ]);
+
+    const user = await this.reviewsRepository.findById([{ user_id: user_id }]);
+
+    if (!user) throw { code: 404, errorMessage: '작성자가 일치하지 않습니다.' };
+
+    if (!review)
+      throw { code: 411, errorMessage: '조건에 맞는 리뷰가 없습니다.' };
+
+    await this.reviewsRepository.updateOne({ content, rate });
+    return { code: 201, message: '리뷰를 수정하였습니다.' };
+  };
+
+  delete = async (user_id, review_id) => {
+    const review = await this.reviewsRepository.findById([
+      { review_id: review_id },
+    ]);
+
+    const user = await this.reviewsRepository.findById([{ user_id: user_id }]);
+
+    if (!user) throw { code: 404, errorMessage: '작성자가 일치하지 않습니다.' };
+
+    if (!review)
+      throw { code: 411, errorMessage: '조건에 맞는 리뷰가 없습니다.' };
+
+    await this.reviewsRepository.destroy();
+    return { code: 201, message: '리뷰를 삭제하였습니다.' };
+  };
+}
+
+module.exports = ReviewsService;
